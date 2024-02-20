a14bdebc719d7fb5e822e0b247ecd75610216623
Vaibhav Pathak <vaibhavpathak852947@gmail.com>
2024-02-20 19:28:53 +0530

    update rating validation

--- validators/rating.validator.js
+++ validators/rating.validator.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
 const isValidForCreate = Joi.object({
-    products: Joi.array().items(Joi.object({
+    items: Joi.array().items(Joi.object({
         productId: Joi.objectId().required(),
         itemId: Joi.objectId().required(),
         rating: Joi.number().min(1).max(5).required()
